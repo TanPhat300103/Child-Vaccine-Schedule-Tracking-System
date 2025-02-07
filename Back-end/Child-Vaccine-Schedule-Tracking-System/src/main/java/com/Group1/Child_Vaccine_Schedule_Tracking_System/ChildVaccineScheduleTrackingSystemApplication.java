fca20dc2cfc9148fdcae5dd057257bb78959dddd
HuuKhang <khangdeptrai198@gmail.com>
2025-02-07 21:43:20 +0700

    Create Project SpringBoot

---
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/com/Group1/Child_Vaccine_Schedule_Tracking_System/ChildVaccineScheduleTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Group1.Child_Vaccine_Schedule_Tracking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildVaccineScheduleTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildVaccineScheduleTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/test/java/com/Group1/Child_Vaccine_Schedule_Tracking_System/ChildVaccineScheduleTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group1.Child_Vaccine_Schedule_Tracking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildVaccineScheduleTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
