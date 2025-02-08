8c4c9ecfe18e6fbe4e1a5c0765bd242b54538a98
HuuKhang <khangdeptrai198@gmail.com>
2025-02-08 21:12:29 +0700

    Use Spring Initialzr to create Project Springboot

---
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/org/gr1fpt/childvaccinescheduletrackingsystem/Student.java
@@ -0,0 +1,11 @@
+package org.gr1fpt.childvaccinescheduletrackingsystem;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Student {
+    public int age;
+    public String name;
+}
---
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/test/java/org/gr1fpt/childvaccinescheduletrackingsystem/ChildVaccineScheduleTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gr1fpt.childvaccinescheduletrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildVaccineScheduleTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
