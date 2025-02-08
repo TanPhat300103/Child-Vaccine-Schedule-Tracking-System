50b0172da1831f346cd0fc56b9412fed78fcb340
Thu Ha <hanptse184261@fpt.edu.vn>
2025-02-08 21:02:28 +0700

    test lombok

--- Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/com/Group1/Child_Vaccine_Schedule_Tracking_System/ChildVaccineScheduleTrackingSystemApplication.java
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/com/Group1/Child_Vaccine_Schedule_Tracking_System/ChildVaccineScheduleTrackingSystemApplication.java
@@ -1,7 +1,6 @@
 package com.Group1.Child_Vaccine_Schedule_Tracking_System;
 
 import com.Group1.Child_Vaccine_Schedule_Tracking_System.model.Role;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,7 +8,7 @@ public class ChildVaccineScheduleTrackingSystemApplication {
 
 	public static void main(String[] args) {
 		Role r = new Role(1,"hello");
-		System.out.println(r.toString());
+		System.out.println(r.getRoleName());
 	}
 
 }
