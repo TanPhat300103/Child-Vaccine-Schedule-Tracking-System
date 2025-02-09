16ee197bdbab5e1b97f73da90e3a044842eed5a4
HuuKhang <khangdeptrai198@gmail.com>
2025-02-09 18:47:22 +0700

    Done Customer, CRUD, Exception, Validaton

--- Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/org/gr1fpt/childvaccinescheduletrackingsystem/model/Customer.java
+++ Back-end/Child-Vaccine-Schedule-Tracking-System/src/main/java/org/gr1fpt/childvaccinescheduletrackingsystem/model/Customer.java
@@ -1,5 +1,9 @@
 package org.gr1fpt.childvaccinescheduletrackingsystem.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +12,12 @@ import java.sql.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "Customer") // Chỉ định tên bảng là 'Human'
 public class Customer {
-
+    @Id
     private String customerId;
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone should be valid")
     private String phoneNumber;
     private String firstName;
     private String lastName;
@@ -19,7 +26,10 @@ public class Customer {
     private String password;
     private String address;
     private String banking;
+    @Email(message = "Email should be valid")
     private String email;
+    @Min(value = 1,message = "Invalid Id")
+    @Max(value = 3,message = "Invalid Id")
     private int roleId;
     private boolean active;
 }
