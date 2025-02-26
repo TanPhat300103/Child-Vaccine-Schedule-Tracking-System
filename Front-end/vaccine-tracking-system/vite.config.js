f57ef73fd17ff0f534ecad74b0970f4278e98b45
Dev Wibu <tantnse184055@fpt.edu.vn>
2025-02-26 19:42:33 +0700

    Remove commented-out proxy configuration in Vite setup

--- Front-end/vaccine-tracking-system/vite.config.js
+++ Front-end/vaccine-tracking-system/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/customer": {
-        target: "http://localhost:8080", // Địa chỉ API server của bạn
-        changeOrigin: true,
-        secure: false, // Nếu bạn đang dùng HTTP mà không phải HTTPS
-      },
+      // "/customer": {
+      //   target: "http://localhost:8080", // Địa chỉ API server của bạn
+      //   changeOrigin: true,
+      //   secure: false, // Nếu bạn đang dùng HTTP mà không phải HTTPS
+      // },
     },
   },
 });
